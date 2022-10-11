1821e25b6c4e103bec1d7e7b0f9201d5bff08c9e
Michael Holmes <developer@techintco.com>
2022-10-10 22:04:52 -0500

    Task 4 completed

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -7,6 +7,7 @@ from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Read the airline data into pandas dataframe
 spacex_df = pd.read_csv("spacex_launch_dash.csv")
@@ -17,7 +18,7 @@ min_payload = spacex_df['Payload Mass (kg)'].min()
 sites = spacex_df['Launch Site'].unique()
 site_dicts = [{'label': 'All', 'value': 'ALL'}]
 for site in sites:
-    site_dicts.append({'label': site, 'Values': site})
+    site_dicts.append({'label': site, 'value': site})
 
 # Create a dash application
 app = dash.Dash(__name__)
@@ -42,19 +43,64 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 
                                 html.P("Payload range (Kg):"),
                                 # TASK 3: Add a slider to select payload range
-                                # dcc.RangeSlider(id='payload-slider',...)
+                                dcc.RangeSlider(id='payload-slider',
+                                                min=0, max=10000, step=1000,
+                                                marks={0: '0',
+                                                       2500: '2500',
+                                                       5000: '5000',
+                                                       7500: '7500',
+                                                       10000: '10000'},
+                                                value=[min_payload, max_payload]),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
                                 ])
 
+
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+@app.callback(Output(component_id='success-pie-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+def get_pie_chart(entered_site):
+    if entered_site == 'ALL':
+        fig = px.pie(spacex_df, values='class',
+                     names='Launch Site',
+                     title='Total Success Launches by Site')
+    else:
+        # return the outcomes pie chart for a selected site
+        filtered_df = spacex_df[spacex_df['Launch Site'] == entered_site]
+        successes = filtered_df['class'].sum()
+        failures = filtered_df['class'].count() - successes
+
+        fig = px.pie(filtered_df, values=[successes, failures],
+                     names=['1', '0'],
+                     title='Total Success Launches for site ' + entered_site)
+    return fig
+
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(Output(component_id='success-payload-scatter-chart', component_property='figure'),
+              Input(component_id='site-dropdown', component_property='value'))
+# Input(component_id='payload-slider', component_property='value')])
+def get_scatter_chart(entered_site):
+    if entered_site == 'ALL':
+        fig_scatter = px.scatter(spacex_df,
+                                 x='Payload Mass (kg)',
+                                 y='class',
+                                 color="Booster Version Category",
+                                 title="Correlation between Payload and Success for all Sites")
+    else:
+        # return the outcomes pie chart for a selected site
+        filtered_df = spacex_df[spacex_df['Launch Site'] == entered_site]
+        fig_scatter = px.scatter(filtered_df,
+                                 x='Payload Mass (kg)',
+                                 y='class',
+                                 color="Booster Version Category",
+                                 title='Correlation between Payload and Success for ' + entered_site + ' site')
+    return fig_scatter
 
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
